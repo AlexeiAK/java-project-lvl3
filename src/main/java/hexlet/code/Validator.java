4ec3b9ec459c61cdce6619d37ff012168ace1bcd
AlexeiAK <aak3xx@gmail.com>
2022-06-10 15:37:32 +0300

    fix: split validation of same type schemas
    Previous invalid code:
    If you create 2 schemes of the same type, then the validator will store checks for both of them.
    The reason: the return schema — still the same final schema — but is not a new schema.

--- src/main/java/hexlet/code/Validator.java
+++ src/main/java/hexlet/code/Validator.java
@@ -1,24 +1,20 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.StringSchema;
 import hexlet.code.schemas.NumberSchema;
 
 public class Validator {
-    private final BaseSchema stringSchema = new StringSchema();
-    private final BaseSchema numberSchema = new NumberSchema();
-    private final BaseSchema mapSchema = new MapSchema();
 
     public final StringSchema string() {
-        return (StringSchema) stringSchema;
+        return new StringSchema();
     }
 
     public final NumberSchema number() {
-        return (NumberSchema) numberSchema;
+        return new NumberSchema();
     }
 
     public final MapSchema map() {
-        return (MapSchema) mapSchema;
+        return new MapSchema();
     }
 }
