36e8f2f033c5f2690b0e638d66c347e95e3934e0
AlexeiAK <aak3xx@gmail.com>
2022-06-10 15:32:55 +0300

    perf: BaseSchema

--- src/main/java/hexlet/code/schemas/BaseSchema.java
+++ src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,29 +5,18 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class BaseSchema {
-    private final List<Predicate> checks = new ArrayList<>();
+    private final List<Predicate<Object>> checks = new ArrayList<>();
 
     public final boolean isValid(Object testedObject) {
-        Predicate<Object> finalPredicate = null;
+        for (Predicate predicate : checks) {
+            boolean resultOfPredicate = predicate.test(testedObject);
 
-        boolean result = true;
-
-        if (checks.size() == 0) {
-            finalPredicate = o -> o == null || o.equals("");
-        } else {
-            for (Predicate predicate : checks) {
-                finalPredicate = predicate;
-                boolean resultOfPredicate = finalPredicate.test(testedObject);
-
-                // If one of result is false, then the others will be false
-                if (!resultOfPredicate) {
-                    result = false;
-                    break;
-                }
+            if (!resultOfPredicate) {
+                return false;
             }
         }
 
-        return result;
+        return true;
     }
 
     protected final void addPredicate(Predicate predicate) {
